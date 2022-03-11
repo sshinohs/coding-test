fcca3e8849bf2ac65f4b06727ba85f08026ad16d
sshinohs <sshinohs@gmail.com>
2022-03-11 21:58:48 +0900

    그리디_완료

--- 16_그리디_알고리즘/동전_0_11047.py
+++ 16_그리디_알고리즘/동전_0_11047.py
@@ -1,4 +1,23 @@
 n, k = map(int, input().split())
 
-print(n)
-print(k)
+values = []
+for ii in range(n):
+    value = int(input())
+    if value <= k:
+        values.append(value)
+
+count = 0
+change = k
+idx_end = len(values)-1
+idx_now = idx_end
+while(change != 0):
+    if change >= values[idx_now]:
+        cval = change // values[idx_now]
+        change = change % values[idx_now]
+        count = count + cval
+    elif idx_now ==0 :
+        break
+    else:
+        idx_now = idx_now - 1
+
+print(count)
