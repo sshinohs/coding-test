ebaed3137da4da1999d0204c724b594b5ca62d71
sshinohs <sshinohs@gmail.com>
2022-07-16 13:57:06 +0900

    20220716_서로소 집합

---
+++ 이코테/graphTheory/서로소_집합_경로압축.py
@@ -0,0 +1,43 @@
+# 집합을 한번 조회 하면 경로 압축이 된다!
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+v, e = map(int, input().split())
+parent = [0] * (v + 1)
+
+for i in range(1, v + 1):
+    parent[i] = i
+
+for i in range(e):
+    a, b = map(int, input().split())
+    union_parent(parent, a, b)
+
+
+print('부모 테이블1: ', end='')
+for i in range(1, v + 1):
+    print(parent[i], end=' ')
+
+
+print('각 원소가 속한 집합: ', end='')
+for i in range(1, v + 1):
+    print(find_parent(parent, i), end =' ')
+
+
+
+print()
+
+print('부모 테이블2: ', end='')
+for i in range(1, v + 1):
+    print(parent[i], end=' ')
