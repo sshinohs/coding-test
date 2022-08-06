dea0baadc0d95fa4e5508e4f64690c4569924da6
sshinohs <sshinohs@gmail.com>
2022-08-07 08:51:55 +0900

    20220807; codeforce C 수정;

--- codeforces/competetion/no812_div2/C_Build_Permutation.py
+++ codeforces/competetion/no812_div2/C_Build_Permutation.py
@@ -3,20 +3,21 @@ import math
 
 input = sys.stdin.readline
 
-dp = [[], [0], [1, 0]]
+def find_permutation(arr, n):
+    if arr[n] != math.inf:
+        return arr[n]
+    else:
+        check = math.floor(math.sqrt((n-1)*2))
+        return find_permutation(arr, check**2-(n-1)) + list(range((n-1), check**2-(n-1)-1, -1))
+
+dp = [math.inf for _ in range(10**5 + 1)]
+dp[0] = []
+dp[1] = [0]
+dp[2] = [1, 0]
 
 for _ in range(int(input())):
     n = int(input())
-    if n + 1 <= len(dp):
-        for num in dp[n]:
-            print(num, end=' ')
-        print()
-    else:
-        for i in range(len(dp), n+1):
-            check = math.floor(math.sqrt((i-1)*2))
-            dp.append(dp[check**2-(i-1)] + list(range((i-1), check**2-(i-1)-1, -1)))
-        for num in dp[n]:
-            print(num, end=' ')
-        print()
-
-
+    answer = find_permutation(dp, n)
+    for num in answer:
+        print(num, end=' ')
+    print()
