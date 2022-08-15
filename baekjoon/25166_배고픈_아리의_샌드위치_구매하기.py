d886d05832ac979c0e9e07cb7a53d56b388d1026
sshinohs <sshinohs@gmail.com>
2022-08-15 14:12:02 +0900

    20220815; 비트연산

--- baekjoon/25166_배고픈_아리의_샌드위치_구매하기.py
+++ baekjoon/25166_배고픈_아리의_샌드위치_구매하기.py
@@ -4,4 +4,18 @@ input = sys.stdin.readline
 
 S, M = map(int, input().split())
 
-print(bin(M)[2:][::-1])
+if S < 1024:
+    output = 'No thanks'
+else:
+    remain = S - 1023
+    flag = True
+    for i in range(11):
+        checker = 1 << i
+        if (remain & checker) != 0 and (M & checker) == 0:
+            flag = False
+    if flag:
+        output = 'Thanks'
+    else:
+        output = 'Impossible'
+
+print(output)
