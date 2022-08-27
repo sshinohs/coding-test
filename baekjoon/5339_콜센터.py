32ed44bd55a999850776915042c19ec2b13f0e88
sshinohs <sshinohs@gmail.com>
2022-08-28 00:37:11 +0900

    20220828

---
+++ baekjoon/5339_콜센터.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+print("     /~\\")
+print("    ( oo|")
+print("    _\\=/_")
+print("   /  _  \\")
+print("  //|/.\|\\\\")
+print(" ||  \\ /  ||")
+print("============")
+print("|          |")
+print("|          |")
+print("|          |")
